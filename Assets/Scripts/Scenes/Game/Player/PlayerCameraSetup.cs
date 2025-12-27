fcb86c4967d7299f7a4112d69adada5b3129ade2
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-12-27 17:05:15 -0500

    Arreglo de Camara 3ra Persona
    Se hizo que segun que player seas la camara e utilice en dicho player el resto no lo usara evitando problemas

---
+++ Assets/Scripts/Scenes/Game/Player/PlayerCameraSetup.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Cinemachine;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerCameraSetup : NetworkBehaviour
+{
+    [SerializeField] private CinemachineCamera freeLook;
+    [SerializeField] private GameObject playerCamera;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            // No es mi player → apago cámara
+            freeLook.gameObject.SetActive(false);
+            return;
+        }
+
+        // Es MI player
+        freeLook.gameObject.SetActive(true);
+    }
+}
